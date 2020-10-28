9bc9640c02851194eba1341c8255b7f62ec13566
KloderGit <idzhyan.develop@outlook.com>
2020-10-28 16:01:06 +0300

    Добавьте файлы проекта.

---
+++ Kloder.Utilities/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Kloder.Utilities
+{
+    public class Class1
+    {
+    }
+}
---
+++ Kloder.Utilities/DataAnnotation/AtLeastOneAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Kloder.Utilities.DataAnnotation
+{
+    public class AtLeastOneAttribute : ValidationAttribute
+    {
+        public Expression MyProperty { get; set; }
+
+        public AtLeastOneAttribute(Expression<Func<Object, Object>> param)
+        {
+            MyProperty = param;
+        }
+
+
+        public override bool IsValid(object value)
+        {
+            return true;
+        }
+    }
+}
