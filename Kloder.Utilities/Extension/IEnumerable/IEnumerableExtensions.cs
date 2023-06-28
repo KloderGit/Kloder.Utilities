77ec822786910f360600cf443dcb4887759fff07
Илья Иджян <KloderGit@users.noreply.github.com>
2023-06-28 15:27:33 +0200

    ext Ienumerable

--- Kloder.Utilities/Extension/IEnumerable/IEnumerableExtensions.cs
+++ Kloder.Utilities/Extension/IEnumerable/IEnumerableExtensions.cs
@@ -7,6 +7,35 @@ namespace Kloder.Utilities.Extension.IEnumerable
 {
     public static class IEnumerableExtensions
     {
+        public static bool AreSequencesEqual(this System.Collections.IEnumerable first, System.Collections.IEnumerable second)
+        {
+            if (first == null && second == null) return true;
+            if (first == null && second != null) return false;
+            if (first != null && second == null) return false;
+
+            var enumerator1 = first.GetEnumerator();
+            var enumerator2 = second.GetEnumerator();
+
+            while (enumerator1.MoveNext())
+            {
+                if (!enumerator2.MoveNext() || !enumerator1.Current.Equals(enumerator2.Current))
+                    return false;
+            }
+
+            if (enumerator2.MoveNext())
+                return false;
+
+            return true;
+        }
+    
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> array)
+        {
+            if (array == null) return true;
+            if (array.Any() == false) return true;
+
+            return false;
+        }
+        
         /// <summary>
         /// Коллекция содержит элементы
         /// </summary>
@@ -15,18 +44,7 @@ namespace Kloder.Utilities.Extension.IEnumerable
         /// <returns></returns>
         public static bool IsFilled<T>(this IEnumerable<T> array)
         {
-            return array != null && array.Any() != false;
-        }
-
-        /// <summary>
-        /// Коллекция не содержит элементов или не создана
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="array"></param>
-        /// <returns></returns>
-        public static bool IsNullOrEmpty<T>(this IEnumerable<T> array)
-        {
-            return array == null || array.Any() == false;
+            return IsNullOrEmpty(array) == false;
         }
     }
 }
